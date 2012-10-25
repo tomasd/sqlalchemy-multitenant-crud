dd95047b072f64c1280b5ebaf08888b5d9c6cf2f
Tomas Drencak <tomas@drencak.com>
2012-10-25 17:01:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlalchemy-crud',
-    version='',
+    version='0.1',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     url='',
     license='',
